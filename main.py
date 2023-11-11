837f834b283a3e5ffd432d72fa0e5d3aa26c757a
Nelson Rodrigues <nelsonrds95@gmail.com>
2023-11-11 22:25:21 +0000

    add rabbitmq

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import pika
+
+rabbitmq_host = os.environ.get("RABBITMQ_HOST", 'localhost')
+rabbitmq_queue = os.environ.get("RABBITMQ_QUEUE", 'laws')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host))
+channel = connection.channel()
+channel.queue_declare(queue=rabbitmq_queue)
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+
 
 URL = "https://www.parlamento.pt/ArquivoDocumentacao/Paginas/Arquivodevotacoes.aspx"
 
